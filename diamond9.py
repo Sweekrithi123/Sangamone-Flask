0c63d48bb76533f6811664b5c3519923475e6297
Sweekrithi123 <99883562+Sweekrithi123@users.noreply.github.com>
2022-03-19 23:16:20 +0530

    Add files via upload

---
+++ diamond9.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,request
+diamond9=Flask(__name__)
+@diamond9.route("/",methods=["POST","GET"])
+def diamond():
+    s1="Sweekrithi"
+    output1=""
+    len1=len(s1)
+    for i in range(1,len1+1,1):
+        output1=output1+s1[0:i]+"\n"
+        
+    for i in range(1,len1,1):
+        output1=output1+s1[0:len1-i]+"\n"
+    return render_template("diamond7.html",message1=output1)
+if __name__=="__main__":
+    diamond9.run(debug=True)
